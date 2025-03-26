6a2be4d32ec33b80d7182c4fb12cedaab6ea1318
Álvaro Buj <45070099+buj8@users.noreply.github.com>
2025-03-26 14:28:22 +0100

    Time: 7 ms (38.61%) | Memory: 17.8 MB (43.83%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [defaultdict(bool) for _ in range(9)]
+        cols = [defaultdict(bool) for _ in range(9)]
+        boxes= [defaultdict(bool) for _ in range(9)]
+
+        for row_id in range(9):
+            for col_id in range(9):
+                num = board[row_id][col_id]
+                if num != ".":
+
+                    box_id = math.floor(row_id/3)*3 + math.floor(col_id/3)
+                    if rows[row_id][num] or cols[col_id][num] or boxes[box_id][num]:
+                        return False
+                    rows[row_id][num], cols[col_id][num], boxes[box_id][num] = True, True, True
+        
+        return True
+
+                    
