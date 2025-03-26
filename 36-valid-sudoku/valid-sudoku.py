403098d415d03201815adc2b88ca31f7684960a4
Álvaro Buj <45070099+buj8@users.noreply.github.com>
2025-03-26 14:31:22 +0100

    Time: 7 ms (38.61%) | Memory: 17.8 MB (69.52%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -8,7 +8,7 @@ class Solution:
             for col_id in range(9):
                 num = board[row_id][col_id]
                 if num != ".":
-                    box_id = math.floor(row_id/3)*3 + math.floor(col_id/3)
+                    box_id = (row_id//3)*3 + (col_id//3)
                     if rows[row_id][num] or cols[col_id][num] or boxes[box_id][num]:
                         return False
                     rows[row_id][num], cols[col_id][num], boxes[box_id][num] = True, True, True
