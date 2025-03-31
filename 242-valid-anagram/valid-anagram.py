2bff07fde5a46ce1caffc08468b1702aac5386f8
Álvaro Buj <45070099+buj8@users.noreply.github.com>
2025-03-31 15:40:32 +0200

    Time: 7 ms (89.56%) | Memory: 18.1 MB (25.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,4 +2,13 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         counterS = Counter(s)
         counterT = Counter(t)
-        return counterS == counterT
+
+        def are_counters_equal(a, b) -> bool:
+            if len(a) != len(b):
+                return False
+            for k, v in a.items():
+                if a[k] != b[k]:
+                    return False
+            return True
+
+        return are_counters_equal(counterS, counterT)
