a87bb534f18f585c199c176f60e96be3442e9ad9
Álvaro Buj <45070099+buj8@users.noreply.github.com>
2025-04-07 14:07:39 +0200

    Time: 0 ms (100.00%) | Memory: 18.1 MB (26.10%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,32 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # BFS
+        
+        h, w = len(image), len(image[0])
+        
+        visited = set()
+        queue = [(sr, sc)]
+        base_color = image[sr][sc]
+
+        def neighbors(r, c):
+            candidates = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+            res = []
+            for candidate in candidates:
+                if (
+                    candidate[0] >= 0 and candidate[0] < h 
+                    and candidate[1] >= 0 and candidate[1] < w
+                    and image[candidate[0]][candidate[1]] == base_color 
+                ):
+                    res.append(candidate)
+            return res
+
+        while len(queue) > 0:
+            r, c = queue.pop(0)
+            if (r, c) not in visited:
+                visited.add((r, c))
+                image[r][c] = color
+                for neighbor in neighbors(r, c):
+                    queue.append(neighbor)
+
+        return image
+        
