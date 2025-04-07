38e6185bf12532a0283824414fdd450dbff431b2
Álvaro Buj <45070099+buj8@users.noreply.github.com>
2025-04-07 10:04:37 +0200

    Time: 0 ms (100.00%) | Memory: 18 MB (68.06%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -10,7 +10,8 @@ class Solution:
 
         while left <= right:
             # We get our mid point and check if it's the target
-            mid = left + (right - right) // 2
+            mid = left + (right - left) // 2
+            print(f"L:{left}, R:{right}, MID:{mid}")
             if nums[mid] == target:
                 return mid
 
@@ -22,9 +23,19 @@ class Solution:
                     right = mid - 1
 
             # If it's not sorted, we look for the half that contains the target
-            #elif target > nums[right] and 
             else:
-                left += 1
+                # Left side is sorted
+                if nums[left] <= nums[mid]:
+                    if target >= nums[left] and target < nums[mid]:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                # Right side is sorted
+                else:
+                    if target <= nums[right] and target > nums[mid]:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
 
 
         return -1
