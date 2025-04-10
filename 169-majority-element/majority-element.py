8e5f8ce8e8c0eb8d6a32e4145026497f5132a012
Álvaro Buj <45070099+buj8@users.noreply.github.com>
2025-04-10 13:46:39 +0200

    Time: 4 ms (70.62%) | Memory: 19.3 MB (87.46%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,15 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # Booyer-Moore Majority Vote Algorithm
-        count = 1
-        candidate = nums[0]
-        for i in range(1, len(nums)):
-            if candidate == nums[i]:
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif candidate == num:
                 count += 1
             else:
                 count -= 1
-                if count == 0:
-                    candidate = nums[i]
-                    count = 1
+                
         return candidate
-
