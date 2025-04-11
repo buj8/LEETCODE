3f587a9e7325fdd4f33bdbda55d4fb9cb74b0d0b
Álvaro Buj <45070099+buj8@users.noreply.github.com>
2025-04-11 11:21:14 +0200

    Time: 35 ms (76.37%) | Memory: 17.7 MB (74.05%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,8 +1,20 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        minlen = min(len(word1), len(word2))
-        res = ""
-        for i in range(minlen):
-            res += word1[i] + word2[i]
+        n1, n2 = len(word1), len(word2)
+        # Pre-allocate list with the exact size needed
+        chars = [''] * (n1 + n2)
         
-        return res + word1[minlen:] + word2[minlen:]
+        # Fill the list with alternating characters
+        for i in range(min(n1, n2)):
+            chars[i*2] = word1[i]
+            chars[i*2+1] = word2[i]
+        
+        # Add remaining characters from the longer word
+        if n1 > n2:
+            for i in range(n2, n1):
+                chars[n2*2 + (i-n2)] = word1[i]
+        else:
+            for i in range(n1, n2):
+                chars[n1*2 + (i-n1)] = word2[i]
+        
+        return ''.join(chars)
