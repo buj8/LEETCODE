c27c421c2b9a5d02920bd03c0f5dfc3eceae142c
Álvaro Buj <45070099+buj8@users.noreply.github.com>
2025-04-13 16:43:45 +0200

    Time: 4 ms (19.48%) | Memory: 17.7 MB (75.19%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,40 +1,23 @@
-class LetterCount(object):
-    def __init__(self, letter, count):
-        self.letter = letter
-        self.count = count
-
-    def __repr__(self):
-        return f"{self.letter}: {self.count}"
-
-    def __lt__(self, other):
-        return self.count > other.count
-
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        # IDEA:
-        # MaxHeap ordered by the amount of letters left
-        #   [example] > "aaaabbbccde" [4, 3, 2, 1, 1]
-        # We pick the top one, append it's value and store it as "latest"
-        # Repeat the same on the next iteration and
         counter = Counter(s)
-        maxHeap = []
-
+        maxheap = []
         for item in counter:
-            lcount = LetterCount(item, counter[item])
-            heapq.heappush(maxHeap, lcount)
-
+            heapq.heappush(maxheap, [-counter[item], item])
+        
+        prev = None
         res = ""
+        while maxheap:
+            curr = heapq.heappop(maxheap)
+            curr[0] += 1
+            res += curr[1]
+            if prev:
+                heapq.heappush(maxheap, prev)
+            if curr[0] < 0:
+                prev = curr
+            else:
+                prev = None
 
-        latest = None
-        while maxHeap:
-            nextLC = heapq.heappop(maxHeap)
-            res += nextLC.letter
-            if latest and latest.count > 0:
-                heapq.heappush(maxHeap, latest)
-            nextLC.count -= 1
-            latest = nextLC
-        
-        return res if len(res) == len(s) else ""
-
-
-        
+        if len(res) == len(s):
+            return res
+        return ""
