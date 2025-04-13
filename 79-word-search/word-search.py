461205a9c2c5c2c79533d9d2367d5e580f630743
Álvaro Buj <45070099+buj8@users.noreply.github.com>
2025-04-13 23:11:29 +0200

    Time: 7796 ms (5.00%) | Memory: 18.1 MB (20.97%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,42 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        def getNeighbors(x, y, seen):
+            candidates = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+            neighbors = []
+            for candidate in candidates:
+                if (
+                    candidate not in seen
+                    and candidate[0] >= 0 and candidate[0] < len(board)
+                    and candidate[1] >= 0 and candidate[1] < len(board[0])
+                ):
+                    neighbors.append(candidate)
+            return neighbors
+                    
+        def dfs(x, y, i, seen):
+            _seen = seen.copy()
+            _seen.add((x,y))
+
+            if board[x][y] != word[i]:
+                return
+
+            if i == len(word) - 1:
+                return True
+            
+            neighbors = getNeighbors(x, y, seen)
+
+            if not neighbors:
+                return
+
+            paths = []
+            for n in neighbors:
+                paths.append(dfs(n[0], n[1], i + 1, _seen))
+
+            return any(paths)
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(i, j, 0, set()):
+                    return True
+
+        return False
