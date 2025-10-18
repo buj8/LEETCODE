81ea59ec634e16dfab04516b7810a01be98f29ae
Álvaro Buj <45070099+buj8@users.noreply.github.com>
2025-10-19 00:19:32 +0200

    Time: 0 ms (100.00%) | Memory: 2.5 MB (21.82%) - LeetSync

---
+++ 1-two-sum/two-sum.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut seen = HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            let needed = target - num;
+            if let Some(&j) = seen.get(&needed) {
+                return vec![j as i32, i as i32];
+            }
+            seen.insert(num, i);
+        }
+        vec![]
+    }
+}
